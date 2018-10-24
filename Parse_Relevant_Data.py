0aaba37e1144cdc90220bca8efbec5c83da90f20
Seanie96 <semcdona@tcd.ie>
2018-10-24 23:50:34 +0100

    Change keys and secrets.

--- Parse_Relevant_Data.py
+++ Parse_Relevant_Data.py
@@ -3,30 +3,54 @@ try:
 except ImportError:
     import simplejson as json
 import twitter
-import pprint
+import time
 
-ACCESS_KEY = '910479255993274368-s7ugR4zq1DlECaqfbhyWyBqUVQVuDUm'
-ACCESS_SECRET = 'htN53B6O8iiTW8kJ3Lb22J7yTPgdX3mpOqalKcvI3Za3T'
-CONSUMER_KEY = 'dfcQU9kdwc2gJBKribuVs6Fx2'
-CONSUMER_SECRET = 'z8iBmWI08BdLcA8FB7HtoRpN6PbN7FaFNUM8hxHz3oln7XauCr'
+ACCESS_KEY = 'xxx'
+ACCESS_SECRET = 'xxx'
+CONSUMER_KEY = 'xxx'
+CONSUMER_SECRET = 'xxx'
 
 def main():
     api = twitter.Api(consumer_key=CONSUMER_KEY,
                       consumer_secret=CONSUMER_SECRET,
                       access_token_key=ACCESS_KEY,
                       access_token_secret=ACCESS_SECRET)
-    
+
     allUsers = []
-    newDict= {}    
+    newDict= {}
     i = 0
     with open('data/gender.json') as f:
+        if i % 1000 == 0:
+            print('waiting for 15 mins.')
+            time.slep(900)
+            print('watining finished.')
         data = json.load(f)
         copy = dict(data)        
         for key in data:
             try:
-                
-            except:
+                i = i + 1      
+                tmp = api.GetUser(key)
+                newDict['created_at'] = tmp.created_at
+                newDict['description'] = tmp.description
+                newDict['favourites_count'] = tmp.favourites_count
+                newDict['id'] = tmp.id
+                newDict['lang'] = tmp.lang
+                newDict['listed_count'] = tmp.listed_count
+                newDict['location'] = tmp.location
+                newDict['name'] = tmp.name
+                newDict['profile_background_color'] = tmp.profile_background_color
+                newDict['profile_link_color'] = tmp.profile_link_color
+                newDict['profile_sidebar_border_color'] = tmp.profile_sidebar_border_color
+                newDict['profile_sidebar_fill_color'] = tmp.profile_sidebar_fill_color
+                newDict['profile_text_color'] = tmp.profile_text_color
+                newDict['screen_name'] = tmp.screen_name
+                newDict['tweet'] = tmp.status.text
+                newDict['gender'] = data[key]
+                allUsers.append(newDict)
+                print(i)
+            except Exception as e:
                 del copy[key]
+                print(e)
 
         with open('data/gender2.json', 'w') as f:
             json.dump(copy, f)
