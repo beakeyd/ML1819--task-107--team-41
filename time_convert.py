2d21cc3b10706a7e2b42271b28236d9b022fd2b3
mccanns3 <mccanns3@tcd.ie>
2018-10-26 17:23:19 +0100

    Conversion script from UTC to Posix Time

--- time_convert.py
+++ time_convert.py
@@ -5,5 +5,12 @@ except ImportError:
 
 
 def main():
-    with open('data/twitter_gender_data.json') as data:
+    with open('data/twitter_gender_data.json', 'r') as data:
         data = json.load(data)
+
+        for item in data:
+            date = dt.strptime(item['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
+            item['created_at'] = time.mktime(date.timetuple())
+
+    with open('data/twitter_gender_data2.json', 'w') as output:
+        json.dump(json_data, output)
