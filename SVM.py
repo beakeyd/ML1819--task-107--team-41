2a1c19bcb20b438444f3168adf95143021438e90
beakeyd <dbeakey@tcd.ie>
2018-10-29 10:07:26 +0000

    updating plots

--- SVM.py
+++ SVM.py
@@ -104,6 +104,7 @@ def main():
         combinedTextFeatures("name", "screen_name", df)
         combinedNumericFeatures("name", "created_at", df)
         combinedTextFeatures("tweet", "description", df)
+        combinedThreeTextFeatures("tweet", "name", "description", df)
         
         
 
@@ -132,14 +133,14 @@ def plotAccuracy(created_at_acc, favourites_acc,
     graph = 'plots/' + graph_name + '.png'
     plt.show()
 
-def plotSingleFeatureData(X, actY, predY, graph_name):
+def plotSingleFeatureData(X, actY, predY, graph_name, xLabel):
     fig, ax = plt.subplots(figsize=(12,2))
     ax.scatter(X, actY, label='Data', marker='+')
     ax.scatter(X, predY, label='Prediction', marker='x')
 
     ax.set_xlabel('Test Feature')
     ax.set_ylabel('Gender')
-    ax.set_title('Feature Plot')
+    ax.set_title(graph_name)
     graph = 'plots/' + graph_name + '.png'    
     fig.savefig(graph)
 
@@ -172,7 +173,7 @@ def created_at_model(created_at, y):
     # create Model
     (X, Xscale) = normaliseData(np.array(created_at).reshape(-1,1))
     Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.1, random_state=42)
-    clf = svm.NuSVC(kernel='poly')
+    clf = svm.NuSVC(kernel='poly', nu=.99)
    
     clf.fit(Xtrain, ytrain)
    # getCAndGamma(clf, Xtrain, ytrain, 'created_at_model')
@@ -181,7 +182,7 @@ def created_at_model(created_at, y):
     #plot data, get and return accuracy of model
     print('created_at Model metrics: ')
     print(classification_report(ytest, predY))
-    plotSingleFeatureData(Xtest, ytest, predY, 'Created_At')
+    plotSingleFeatureData(Xtest, ytest, predY, 'Created_At', 'Posix Time Account Created At - Scaled between 0-1')
     accuracy = accuracy_score(ytest, predY)
     print(str(accuracy))
    # predY=printBestCGamma(clf, Xtrain, ytrain, Xtest, ytest, "single")
@@ -198,7 +199,7 @@ def color_model(profile_background_color, profile_sidebar_fill_color,
     X=np.column_stack((X1, X2))
     # create Model
     Xtrain, Xtest, ytrain, ytest = train_test_split(X, gender, test_size=0.1, random_state=42)
-    clf = svm.NuSVC(kernel='poly')
+    clf = svm.NuSVC(kernel='poly', nu=.7)
     #getCAndGamma(clf, Xtest, ytest, 'color_model')
     clf.fit(Xtrain, ytrain)
 
@@ -218,7 +219,7 @@ def favourites_count_model(favourites_count, y):
     # create Model
     (X, _) = normaliseData(np.array(favourites_count).reshape(-1,1))
     Xtrain, Xtest, ytrain, ytest = train_test_split(X, y,test_size=0.1, random_state=42)
-    clf = svm.NuSVC(kernel='poly', degree=3)
+    clf = svm.NuSVC(kernel='poly', degree=3, nu=.1)
     #getCAndGamma(clf, X, y, 'favourites_count_model')
     clf.fit(Xtrain, ytrain)
     # make predicitions
@@ -226,7 +227,7 @@ def favourites_count_model(favourites_count, y):
     #plot data, get and return accuracy of model
     print('favourites_count Model metrics: ')
     print(classification_report(ytest, predY))
-    plotSingleFeatureData(Xtest, ytest, predY, 'Favourites_Count')
+    plotSingleFeatureData(Xtest, ytest, predY, 'Favourites_Count', 'Number of Favourites on User Tweets - Scaled between 0-1')
 
     accuracy = accuracy_score(ytest, predY)
     print(str(accuracy))
@@ -238,7 +239,7 @@ def listed_count_model(listed_count, y):
     # create Model
     (X, _) = normaliseData(np.array(listed_count).reshape(-1,1))
     Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.1, random_state=42)
-    clf = svm.NuSVC(kernel='poly', degree=3)
+    clf = svm.NuSVC(kernel='poly', degree=3, nu=.1)
     #getCAndGamma(clf, X, y, 'listed_count_model')
     clf.fit(Xtrain, ytrain)
 
@@ -247,7 +248,7 @@ def listed_count_model(listed_count, y):
     #plot data, get and return accuracy of model
     print('listed_count Model metrics: ')
     print(classification_report(ytest, predY))
-    plotSingleFeatureData(Xtest, ytest, predY, 'Listed_Count')
+    plotSingleFeatureData(Xtest, ytest, predY, 'Listed_Count', 'Number of Lists User appears on - Scaled between 0-1')
     
     accuracy = accuracy_score(ytest, predY)
     print(str(accuracy))
@@ -429,15 +430,14 @@ def combinedNumericFeatures(x1, x2,df):
 
     tweet.fit_transform(X_train)
     name.fit_transform(X_train)
-    feats = FeatureUnion([('tweet', tweet), 
-                    
+    feats = FeatureUnion([('tweet', tweet),
                     ('name', name)])
 
     feature_processing = Pipeline([('feats', feats)])
     feature_processing.fit_transform(X_train)
     pipeline = Pipeline([
         ('features',feats),
-        ('classifier', svm.SVC(kernel='linear', C=10, gamma=1)),
+        ('classifier', svm.SVC(kernel='linear', C=1, gamma=1)),
     ])
     
     # Fit and tune model
@@ -477,6 +477,57 @@ def printBestCGamma(model,Xtrain, ytrain,X_test,y_test, feature_count):
     print( np.mean(preds == y_test))
     print(classification_report(y_test, preds))
     return preds
+#The multiple feature text classification code is based off https://www.kaggle.com/baghern/a-deep-dive-into-sklearn-pipelines#
+def combinedThreeTextFeatures(x1, x2, x3,df):
+    print(x1+" and "+x2+" and "+x3)
+    graphName=(str(x1+" and "+x2))
+    
+    features= [c for c in df.columns.values if c   in [x1,x2]]
+    
+    target='gender'
+    
 
+    X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.1, random_state=42)
+    X_train.head()
+    tweet = Pipeline([
+            ('selector', TextSelector(key=x1)),
+            
+            ('words', CountVectorizer(analyzer='word'))
+        ])
+    name= Pipeline([
+            ('selector', TextSelector(key=x2)),
+            
+            ('words', CountVectorizer(analyzer='word'))
+        ])
+    description= Pipeline([
+            ('selector', TextSelector(key=x2)),
+            
+            ('words', CountVectorizer(analyzer='word'))
+        ])
+
+    tweet.fit_transform(X_train)
+    name.fit_transform(X_train)
+    description.fit_transform(X_train)
+    feats = FeatureUnion([('tweet', tweet), 
+                    
+                    ('name', name),
+                    ('description', description)])
+
+    feature_processing = Pipeline([('feats', feats)])
+    feature_processing.fit_transform(X_train)
+    pipeline = Pipeline([
+        ('features',feats),
+        ('classifier', svm.SVC(kernel='linear', C=10, gamma=1)),
+    ])
+    
+    # Fit and tune model
+    pipeline.fit(X_train, y_train)
+    preds=pipeline.predict(X_test)
+    
+    mean=np.mean(preds==y_test)
+    print(mean)
+    print(classification_report(y_test, preds))
+    
+    #printBestCGamma(pipeline, X_train, y_train, X_test, y_test, "multiple")
 if __name__ == '__main__':
     main()
